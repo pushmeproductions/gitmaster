df5bd27cafa6229aaa555c965d0017fb8411b475
kelly-keating <keating.kel@gmail.com>
2017-06-14 17:22:02 +1200

    app made dumb again

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {connect} from 'react-redux'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
 import Game from './Game'
@@ -7,23 +6,14 @@ import Endscreen from './Endscreen'
 import Homepage from './Homepage'
 
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-  }
+const App = () => (
+  <div className='app-container'>
+    <Router>
+      <Route exact path='/' component={Homepage} />
+      <Route path='/game' component={Game} />
+      <Route path='/end' component={Endscreen} />
+    </Router>
+  </div>
+)
 
-  render () {
-    return (
-      <div className='app-container'>
-        <Router>
-          <Route exact path='/' component={Homepage} />
-          <Route path='/game' component={Game} />
-          <Route path='/end' component={Endscreen} />
-        </Router>
-      </div>
-    )
-  }
-
-}
-
-export default connect()(App)
+export default App
