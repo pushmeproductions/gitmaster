ba23345e197b3e87b64b1bc659a04b8de8ea2fcd
ElenaSchubel <ebassist@live.com>
2017-06-14 16:43:09 +1200

    added the UI

---
+++ client/components/UI/Inventory.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+class Inventory extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="inventoryCont">
+        <h2>Hello there I am an Inventory!</h2>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {state}
+}
+
+export default connect(mapStateToProps)(Inventory)
