98575bb5b03a8f08347342af35eec2c4727d9d83
simonol1 <simondanolearyis@gmail.com>
2017-06-15 11:56:51 +1200

    added working link

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -5,9 +5,21 @@ const HomePage = () => {
 
     return (
       <div className = 'home-container'>
-        <h1>GitMaster</h1>
-        <button>
-        <Link to='/game' id='start'>Start</Link>
+        <div className = 'Gitmaster'>
+          <h1>GitMaster</h1>
+        </div>
+        <div className = 'introtxt'>
+          <p>
+            "It is the year 3000. Organic lifeforms battle for freedom
+            against their robot overlords. The Version Control wars
+            have been raging since before you were born.
+            You are the most recent in the line of Gitmasters,
+            and have been captured after your defeat in the
+            most recent Merge Conflict. You awaken in a cell…"
+          </p>
+        </div>
+        <button id='start-button'>
+          <Link to='/game' id='start'>Start</Link>
         </button>
       </div>
     )
