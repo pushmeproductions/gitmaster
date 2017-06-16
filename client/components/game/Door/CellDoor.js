4389ea0342bf555794ee7b0f8ffe7cd0d6002c45
ElenaSchubel <ebassist@live.com>
2017-06-16 12:16:47 +1200

    have added an exit msg

---
+++ client/components/game/Door/CellDoor.js
@@ -0,0 +1,31 @@
+import Door from './Door'
+
+
+export default class CellDoor extends Door {
+  constructor(dispatch) {
+    super(dispatch)
+    this.img = 'images/items/Door.png'
+    this.msg = 'The door is locked...'
+    this.name = 'cellDoor'
+    this.exit = 'Spark! Flash! The door opens and you find yourself in a corridor'
+    this.roomStyle = {
+      width: '680px',
+      height: '450px',
+      position: absolute,
+      top: 0,
+      left: 0,
+      z: 5
+    }
+  }
+  mouseClick() {
+
+  }
+
+  mouseOver() {
+
+  }
+
+  mouseOff(){
+
+  }
+}
---
+++ client/components/game/Door/Door.js
@@ -0,0 +1,17 @@
+export default class Door {
+  constructor (dispatch) {
+    this.dispatch = dispatch
+
+  }
+  mouseClick() {
+
+  }
+
+  mouseOver() {
+
+  }
+
+  mouseOff() {
+
+  }
+}
