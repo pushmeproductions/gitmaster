51974b741373574ea9a93e3c1952f8327c27b589
simonol1 <simondanolearyis@gmail.com>
2017-06-18 15:06:58 +1200

    we have 12 passing tests

--- client/components/game/items/Authbot.js
+++ client/components/game/items/Authbot.js
@@ -4,15 +4,15 @@ import {updateLog} from '../../../actions/currentLog'
 export default class Authbot extends Item {
   constructor (dispatch) {
     super (dispatch)
-    this.img = 'images/items/Authbot.png'
+    this.img = 'images/items/AuthBot.png'
     this.msg = 'Authbot: "AUTHORISE YOURSELF"'
     this.name = 'authbot'
     this.activeStyle = {
-      width: '176px',
-      height: '229px',
+      width: '146px',
+      height: '220px',
       position: 'absolute',
-      top: '28%',
-      left: '34%',
+      top: '31%',
+      left: '29%',
       zIndex: 5
     }
     this.idleStyle =  {
