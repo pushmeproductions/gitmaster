3dd072d8fccf53446a8dbeee1555eb5aa7444a73
simonol1 <simondanolearyis@gmail.com>
2017-06-19 09:31:31 +1200

    making lint changes

--- client/components/game/code/Locker2.js
+++ client/components/game/code/Locker2.js
@@ -1,7 +1,5 @@
 import CodeGiver from './CodeGiver'
 
-import {sendToFunc} from '../../../actions/sendToFunction'
-
 export default class Locker2 extends CodeGiver {
   constructor(dispatch) {
     super(dispatch)
--- client/components/game/code/Moustache.js
+++ client/components/game/code/Moustache.js
@@ -1,6 +1,5 @@
 import CodeGiver from './CodeGiver'
 
-import {sendToFunc} from '../../../actions/sendToFunction'
 
 export default class Moustache extends CodeGiver {
   constructor(dispatch) {
