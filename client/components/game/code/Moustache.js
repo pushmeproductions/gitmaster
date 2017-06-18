5348a0fee25e9faee2ad45a8fc381a4919acd2b0
hannahbeale <hgcbeale@gmail.com>
2017-06-18 20:08:56 +1200

    added static images to public

--- client/components/game/code/Moustache.js
+++ client/components/game/code/Moustache.js
@@ -1,26 +1,43 @@
-import CodeGiver from './CodeGiver'
-
-import {sendToFunc} from '../../../actions/sendToFunction'
-
-export default class Moustache extends CodeGiver {
-  constructor(dispatch) {
-    super(dispatch)
-    this.code = 'code5'
-  }
-
-  mouseClick() {
-
-  }
-
-  mouseOver() {
-
-  }
-
-  mouseOff() {
-
-  }
-
-  send2Func() {
-
-  }
-}
+// import CodeGiver from './CodeGiver'
+//
+// import {sendToFunc} from '../../../actions/sendToFunction'
+//
+// export default class Moustache extends CodeGiver {
+//   constructor(dispatch) {
+//     super(dispatch)
+//     this.code = 'code5'
+//     this.img = 'images/function/empty/closingBrace.png'
+//     this.activeStyle = {
+//       width: '146px',
+//       height: '220px',
+//       position: 'absolute',
+//       top: '31%',
+//       left: '29%',
+//       zIndex: 5
+//     }
+//     this.idleStyle =  {
+//       width: '176px',
+//       height: '230px',
+//       position: 'absolute',
+//       top: '41%',
+//       left: '1%',
+//       zIndex: 5
+//     }
+//   }
+//
+//   mouseClick() {
+//
+//   }
+//
+//   mouseOver() {
+//
+//   }
+//
+//   mouseOff() {
+//
+//   }
+//
+//   send2Func() {
+//
+//   }
+// }
