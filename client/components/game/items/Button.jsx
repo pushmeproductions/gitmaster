ddbc56c03c9da329a41dc514425bc9112901f5d5
simonol1 <simondanolearyis@gmail.com>
2017-06-20 08:48:22 +1200

    added meltdown state to all rooms and a safe message log if the password is correct

--- client/components/game/items/Button.jsx
+++ client/components/game/items/Button.jsx
@@ -6,7 +6,7 @@ export default class Button extends Item {
   constructor(dispatch) {
     super (dispatch)
     this.img = 'images/items/Button.png'
-    this.msg = 'You pushed a button'
+    this.meltdownmsg = 'Noooo....what were you thinking? NEVER push to master! ..Meltdown initiated'
     this.name = 'button'
     this.safemsg = 'Well done...you obviously know what will happen if you push to master'
     this.roomStyle = {
@@ -21,8 +21,8 @@ export default class Button extends Item {
 
   mouseClick(branch) {
     if (branch == 'master') {
-    this.dispatch(meltdown())
-    this.dispatch(updateLog(this.safemsg))
-  } else {this.dispatch(updateLog(this.msg))}
+      this.dispatch(meltdown())
+      this.dispatch(updateLog(this.meltdownmsg))
+  } else {this.dispatch(updateLog(this.safemsg))}
   }
 }
