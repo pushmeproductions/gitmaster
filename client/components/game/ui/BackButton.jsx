050aad2f8f628b0d9024e65a656ba4ce77e591e4
ElenaSchubel <ebassist@live.com>
2017-06-19 15:08:18 +1200

    added more msgs

--- client/components/game/ui/BackButton.jsx
+++ client/components/game/ui/BackButton.jsx
@@ -10,20 +10,26 @@ import {connect} from 'react-redux'
   mouseClick() {
 
     switch(this.props.location) {
+      case 'cell':
+      this.dispatch(updateLog("There is no past only the present."))
+      break;
       case 'corridor':
       this.dispatch(locChange('cell'))
-      this.dispatch(updateLog(this.exitMsg))
+      this.dispatch(updateLog('Why would come back?'))
       break;
       case 'staffroom':
       this.dispatch(locChange('corridor'))
+      this.dispatch(updateLog('You are back in the corridor.'))
       break;
       case 'reactcore':
       this.dispatch(locChange('corridor'))
+      this.dispatch(updateLog('You are back in the corridor.'))
       break;
       case 'escapepod':
       this.dispatch(locChange('reactcore'))
+      this.dispatch(updateLog('You are back in the React-core room. Were you really trying to escape?!'))
       break;
-      return
+
     }
   }
 
