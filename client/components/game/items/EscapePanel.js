153f399880a894b56da7a9cf4a1f1264455aa1f6
hannahbeale <hgcbeale@gmail.com>
2017-06-20 18:32:30 +1200

    debugging'

--- client/components/game/items/EscapePanel.js
+++ client/components/game/items/EscapePanel.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
 import {updateLog} from '../../../actions/currentLog'
-import {locChange} from '../../../actions/door'
-
 
 class EscapePanel extends React.Component {
   constructor(props) {
@@ -35,11 +34,19 @@ mouseClick() {
     }
   }
   render(){
-    return (<img
-      src={this.img}
-      style={this.roomStyle}
-      onClick={() => this.mouseClick()}/>
-    )
+    if(this.props.meltdown){
+      return (<Link to='/end'><img
+        src={this.img}
+        style={this.roomStyle}
+        onClick={() => this.mouseClick()}/></Link>
+      )
+    } else {
+      return (<img
+        src={this.img}
+        style={this.roomStyle}
+        onClick={() => this.mouseClick()}/>
+      )
+    }
   }
 }
 
