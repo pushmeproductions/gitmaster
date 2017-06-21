d9fef70df5575c9868f91932de37d1960e0b20c5
hannahbeale <hannahbeale@users.noreply.github.com>
2017-06-22 09:24:23 +1200

    Update ReactSign.js

--- client/components/game/Door/ReactSign.js
+++ client/components/game/Door/ReactSign.js
@@ -6,7 +6,7 @@ export default class ReactSign extends Door {
   constructor (dispatch) {
     super (dispatch)
     this.img = 'images/items/ReactSign.png'
-    this.exitMsg = 'Welcome to the React Room'
+    this.exitMsg = "You've reached the React Core! Hurry!"
     this.error = 'AuthBot: "You are not AUTHORISED to pass."'
     this.name = 'reactSign'
     this.nextLoc = 'reactcore'
