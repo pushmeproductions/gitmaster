bc8509171f33083ab2fcf7366981233b988438df
kelly-keating <keating.kel@gmail.com>
2017-06-21 16:13:37 +1200

    proptypes

--- client/components/game/items/EscapePanel.js
+++ client/components/game/items/EscapePanel.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
 
 import {updateLog} from '../../../actions/currentLog'
 import {funEnding} from '../../../actions/ending'
@@ -8,19 +7,19 @@ import {funEnding} from '../../../actions/ending'
 class EscapePanel extends React.Component {
   constructor(props) {
   super(props)
-  this.dispatch = props.dispatch
-  this.tryagain = 'Have you been been trying'
-  this.lostMsg = "Hahaha you have been defeat by SVN!!"
-  this.wonMsg = 'Wow you defeated the evil SVN!'
-  this.img = 'images/items/EscKey.png'
-  this.roomStyle = {
-    cursor: 'pointer',
-    width: '58px',
-    height: '47px',
-    position: 'absolute',
-    top: '45%',
-    left: '23%',
-    zIndex: 5
+    this.dispatch = props.dispatch
+    this.tryagain = 'Have you been been trying'
+    this.lostMsg = "Hahaha you have been defeat by SVN!!"
+    this.wonMsg = 'Wow you defeated the evil SVN!'
+    this.img = 'images/items/EscKey.png'
+    this.roomStyle = {
+      cursor: 'pointer',
+      width: '58px',
+      height: '47px',
+      position: 'absolute',
+      top: '45%',
+      left: '23%',
+      zIndex: 5
     }
     this.state = {
       location: this.props.location,
@@ -36,18 +35,14 @@ class EscapePanel extends React.Component {
     })
   }
 
-
   mouseClick() {
     if (this.props.meltdown && this.props.func.length < 5) {
       this.dispatch(updateLog(this.lostMsg))
-      //bad
       window.location.href = '/#/end'
-      console.log("this should be bad ending")
-
+      //bad
     } else if (this.props.meltdown && this.props.func.length == 5) {
       this.dispatch(funEnding())
       window.location.href = '/#/end'
-      console.log("this should be good ending");
       // good
       this.dispatch(updateLog(this.wonMsg))
     } else {
@@ -55,16 +50,11 @@ class EscapePanel extends React.Component {
     }
   }
 
-  renderEscButton() {
+  render(){
     return (<img id='esc'
     src={this.img}
     style={this.roomStyle}
     onClick={() => this.mouseClick()} />
-    )}
-
-  render(){
-    return (
-      this.renderEscButton()
     )
   }
 }
@@ -78,9 +68,6 @@ class EscapePanel extends React.Component {
     }
   }
 
+EscapePanel.propTypes = { func: React.PropTypes.array, meltdown: React.PropTypes.boolean, dispatch: React.PropTypes.object };
+
 export default connect(mapStateToProps)(EscapePanel)
-//
-// <Link to='/end'><img
-//   src={this.img}
-//   style={this.roomStyle}
-//   onClick={() => this.mouseClick()}/></Link>
